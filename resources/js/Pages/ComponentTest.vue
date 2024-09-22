90510f401a1a6a56dee020e993951ea35cbaa91e
hokuson <git config --global user.email>
2024-09-22 23:21:14 +0900

    Inertia-vue.js

---
+++ resources/js/Pages/ComponentTest.vue
@@ -0,0 +1,20 @@
+<script setup>
+import GuestLayout from '@/Layouts/GuestLayout.vue'
+import Label from '@/Components/InputLabel.vue'
+import Input from '@/Components/TextInput.vue'
+
+const emitTest = e => console.log(e)
+
+</script>
+
+
+
+<template>
+<GuestLayout>
+    <label value="件名">タイトル</label><br>
+    <Input 
+    modelValue="初期値が入ります"
+    @update:modelValue="emitTest"
+    ></Input>
+</GuestLayout>
+</template>
